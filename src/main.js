9a6ff7ea6c8ad2da7f54785c4d106cdec80123f8
leonardo oliveira <leonardo.lon@outlook.com>
2023-08-26 20:57:23 -0300

    cabeçalho

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll("[data-faq-question]");
+
+    const heroSection = document.querySelector(".hero");
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener("scroll", function(){
+       const posicaoAtual = window.scrollY;
+
+       if(posicaoAtual < alturaHero){
+        ocultaElementosdoHeader();
+       }else{
+        exibeElementosdoHeader();
+       }
+    })
     
 
     for (let i= 0; i < buttons.length; i++) {
@@ -19,6 +32,16 @@ document.addEventListener('DOMContentLoaded', function(){
     }
 
 })
+
+function ocultaElementosdoHeader(){
+    const header = document.querySelector("header");
+    header.classList.add("header--is-hidden");
+}
+
+function exibeElementosdoHeader(){
+    const header = document.querySelector("header");
+    header.classList.remove("header--is-hidden");
+}
 
 function abreOuFechaResposta(elemento){
     const classe= "faq__questions__item--is-open";
